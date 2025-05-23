b4efbf16ab741ba2a95827dc4dbaaebb0c27f8b1
Ad <93925469+AdrianaRidolfi@users.noreply.github.com>
2025-05-23 13:47:57 +0200

    revert e gestione img

--- quiz_bot/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-TOKEN = os.getenv("TELEGRAM_TOKEN", "Y7861155385:AAEhLcBpmcGvkq_rlxbnwcNSMHNAFWKgb8s")
-QUIZ_FOLDER = os.path.join(os.path.dirname(__file__), "quizzes")
-IMG_FOLDER = os.path.join(QUIZ_FOLDER, "images")
--- quiz_bot/constants.py
+++
@@ -1,4 +0,0 @@
-CALLBACK_STOP = "stop"
-CALLBACK_CHANGE_COURSE = "change_course"
-CALLBACK_RESET_STATS = "reset_stats"
-CALLBACK_REPEAT_QUIZ = "repeat_quiz"
